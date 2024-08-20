0d63346276d54a703645651609a054da8d86440e
Ivan <ivanaleksandrov98@gmail.com>
2024-08-20 15:35:23 +0300

    adding Post for stocks

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b02c66446906df8dfe97f56ab097d3422ff79c05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+009638b77462a11cce17241bb7a41e15d3433830")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
