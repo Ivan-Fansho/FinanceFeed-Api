3f8ab44adc24c5228bf4cba3c7519e134b2e87f3
Ivan <ivanaleksandrov98@gmail.com>
2024-08-20 11:37:37 +0300

    adding Models for Stocks and Comments

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e608999cc4e4bbc3630cdc6be7ed57c629fc9743")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c8e06861238412434a8fea85c2698cef3ddd04")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
