729b5fa9ec529e4c9c929578708235690676524d
Ivan <ivanaleksandrov98@gmail.com>
2024-08-31 14:59:15 +0300

    Adding search for symbol and company name for the stocks

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Models;
 
 namespace api.Interfaces
 {
     public interface IStockRepository
     {
-        Task<List<Stock>> GetAllAsync();
+        Task<List<Stock>> GetAllAsync(QueryObject query);
         Task<Stock?> GetByIdAsync(int id); //FirstorDefault CAN BE NULL
         Task<Stock> CreateAsync(Stock StockModel);
         Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d26535c970385304d7b2c911263fc3cbbbd747f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ba5fb8d7b54ba710ecaf21833c28654e4a5b17")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
