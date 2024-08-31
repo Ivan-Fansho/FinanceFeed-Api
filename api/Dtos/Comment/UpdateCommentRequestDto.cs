83ba5fb8d7b54ba710ecaf21833c28654e4a5b17
Ivan <ivanaleksandrov98@gmail.com>
2024-08-31 14:41:36 +0300

    Adding data validations to the endpoints and Dtos

--- api/Dtos/Comment/UpdateCommentRequestDto.cs
+++ api/Dtos/Comment/UpdateCommentRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,14 @@ namespace api.Dtos.Comment
 {
     public class UpdateCommentRequestDto
     {
+        [Required]
+        [MinLength(4, ErrorMessage = " Title must be 4 characters at least")]
+        [MaxLength(280, ErrorMessage = " Title cannot be over 280 characters")]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(4, ErrorMessage = " Content must be 4 characters at least")]
+        [MaxLength(280, ErrorMessage = " Content cannot be over 280 characters")]
         public string Content { get; set; } = string.Empty;
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6606328ce22d00310d6d29ac564af3fbb7b4312f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d26535c970385304d7b2c911263fc3cbbbd747f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
