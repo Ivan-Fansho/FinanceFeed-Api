2f9c55e24e8a0beec79fcb10822dadbfc1cd420f
Ivan <ivanaleksandrov98@gmail.com>
2024-09-02 12:11:46 +0300

    Adding Login endpoint and authorization

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2fd89ebb3ee9ddff4151e99be147be146a2fc3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bd6fe130ed3390e57780cc52be8f337ddbcaa54")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
